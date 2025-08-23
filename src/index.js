0017797eaca56c8f6ba2fb2e870ce57135ce13ab
ruben <174733720+ruben-23@users.noreply.github.com>
2025-08-23 21:32:45 +0300

    Add event listener for home, menu and about buttons in header

--- src/index.js
+++ src/index.js
@@ -5,8 +5,10 @@ import loadHomePage from './homepage.js';
 import loadMenuPage from './menupage.js';
 import loadAboutPage from './aboutpage.js';
 
-// loadHomePage();
+loadHomePage();
 
-// loadMenuPage();
+document.getElementById('home-button').addEventListener('click', loadHomePage);
 
-loadAboutPage();
+document.getElementById('about-button').addEventListener('click', loadAboutPage);
+
+document.getElementById('menu-button').addEventListener('click', loadMenuPage);
